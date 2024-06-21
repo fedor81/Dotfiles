b79bcca9a0454662ecc2bf1f44b122f37b06ac05
Laroxyss <lomonosov_18@mail.ru>
2024-06-21 09:17:36 +0500

    Доработал скрипт установки

--- install.sh
+++ install.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# TODO: Скрипт прерывается после установки oh-,y-zsh
-git clone git@github.com:fedor81/Dotfiles.git ~
-# TODO: Для клонирования каталог должен быть пустым
+echo "Установка ohmyzsh"
+# Download the installer script
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o ohmyzsh_install.sh
+# Make a backup of the original installer
+cp ohmyzsh_install.sh ohmyzsh_install.sh.bak
+# Remove the line that changes the shell
+sed -i '' '/env zsh -l/d' ohmyzsh_install.sh
+# Run the modified installer
+sh ohmyzsh_install.sh
+
+echo "Клонирование конфига"
+TEMP_DIR=$(mktemp -d)
+git clone https://github.com/fedor81/Dotfiles.git $TEMP_DIR
+mv $TEMP_DIR/* ~
+rm -rf $TEMP_DIR
 
 PACKAGES=(
-neovim
-eza
-thefuck
+    neovim
+    eza
+    thefuck
 
-yazi
-# Следующие плагины нужны для поддержки yazi
-ffmpegthumbnailer
-unarchiver
-jq
-poppler
-fd
+    yazi
+    # Следующие плагины нужны для поддержки yazi
+    ffmpegthumbnailer
+    unarchiver
+    jq
+    poppler
+    fd
 
-# Плагины для быстрого поиска
-ripgrep
-fzf
-zoxide
+    # Плагины для быстрого поиска
+    ripgrep
+    fzf
+    zoxide
 )
 
 # Определение os и установщика пакетов
@@ -37,16 +47,20 @@ else
     INSTALLER="sudo apt-get install -y"
 fi
 
+echo "Установка пакетов"
 for PACKAGE in "${PACKAGES[@]}"
 do
-   $INSTALLER $PACKAGE
+    $INSTALLER $PACKAGE
 done
 
 
-# Менеджер плагинов для tmux
+echo "Установка менеджера плагинов tmux"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-# Синхронизация плагинов nvim
+echo "Синхронизация плагинов nvim"
 nvim --headless +"Lazy! sync" +qa
 nvim --headless "+MasonUpdate" +qa
+
+# Переключение на установленную zsh
+exec zsh -l
