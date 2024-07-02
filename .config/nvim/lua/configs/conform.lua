040a25bd009f6127374856ddee62d58e23e0b1e9
Laroxyss <lomonosov_18@mail.ru>
2024-07-02 23:15:55 +0500

    Удалил lazy-lock.json

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -3,6 +3,7 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    json = { "prettier" },
     htmldjango = { "djlint" },
     python = { "black" },
     cpp = { "clang-format" },
